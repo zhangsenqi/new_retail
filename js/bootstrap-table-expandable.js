323a5ce730211d423647c8a56c0727a1ccaa2bc1
zhangsenqi <422149633@qq.com>
2019-07-31 16:33:45 +0800

    提交

---
+++ js/bootstrap-table-expandable.js
@@ -0,0 +1,20 @@
+(function ($) {
+    $(function () {
+        $('.table-expandable').each(function () {
+            var table = $(this);
+            table.children('thead').children('tr').append('<th></th>');
+            table.children('tbody').children('tr').filter(':odd').hide();
+            table.children('tbody').children('tr').filter(':even').click(function () {
+                $('div.table-expandable-arrow.up').removeClass('up')
+                table.children('tbody').children('tr').filter(':odd').hide();
+                var element = $(this);
+                element.next('tr').toggle('slow');
+                element.find(".table-expandable-arrow").toggleClass("up");
+            });
+            table.children('tbody').children('tr').filter(':even').each(function () {
+                var element = $(this);
+                element.append('<td><div class="table-expandable-arrow"></div></td>');
+            });
+        });
+    });
+})(jQuery); 
