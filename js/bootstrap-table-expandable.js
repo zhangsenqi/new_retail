b122659d928ac5f5afe076d233bd79b29a1ef72f
zhangsenqi <422149633@qq.com>
2019-08-08 20:24:57 +0800

    提交

--- js/bootstrap-table-expandable.js
+++ js/bootstrap-table-expandable.js
@@ -5,11 +5,12 @@
             table.children('thead').children('tr').append('<th></th>');
             table.children('tbody').children('tr').filter(':odd').hide();
             table.children('tbody').children('tr').filter(':even').click(function () {
+                var element = $(this);
+                var is_up = element.children('td').eq(5).children('div').attr('class').indexOf('up');
                 $('div.table-expandable-arrow.up').removeClass('up')
                 table.children('tbody').children('tr').filter(':odd').hide();
-                var element = $(this);
-                element.next('tr').toggle('slow');
-                element.find(".table-expandable-arrow").toggleClass("up");
+                is_up === -1 && element.next('tr').toggle('slow');
+                is_up === -1 && element.find(".table-expandable-arrow").toggleClass("up");
             });
             table.children('tbody').children('tr').filter(':even').each(function () {
                 var element = $(this);
